e0071fb777214df073552fad5feba78709b92d23
atila <atila@atila-Lenovo-G40-70>
2016-12-22 06:16:59 -0200

    Add README

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_erros', 1);
